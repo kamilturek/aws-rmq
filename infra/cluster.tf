6c3385670e6e80291c1f83bf159148799be851d8
Kamil Turek <kamil.turek@hotmail.com>
2021-07-24 20:58:52 +0200

    Create launch configuration for node instances

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -12,3 +12,18 @@ resource "aws_iam_instance_profile" "node_profile" {
   name = "node-profile"
   role = aws_iam_role.node_role.name
 }
+
+resource "aws_launch_configuration" "node_launch_config" {
+  name                 = "node-launch-config"
+  image_id             = data.aws_ami.node.image_id
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.node_profile.id
+  key_name             = "kamil"
+  security_groups      = [aws_security_group.node_sg.id]
+
+  user_data = <<EOF
+#!/bin/bash
+sudo service rabbitmq-server start
+sudo rabbitmqctl set_policy "ha-all" '.*' '{"ha-mode": "all"}' 
+EOF
+}
