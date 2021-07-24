3fec47aad316039e5e20c82d87e3c93952196a78
Kamil Turek <kamil.turek@hotmail.com>
2021-07-24 20:54:39 +0200

    Create AMI data source for node instances

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "node" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["node-ami"]
+  }
+}
+
 resource "aws_iam_instance_profile" "node_profile" {
   name = "node-profile"
   role = aws_iam_role.node_role.name
